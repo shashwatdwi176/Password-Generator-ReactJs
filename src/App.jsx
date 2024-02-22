0c8e53ef16f65dfc82731114aaafdd29f53bf4b9
shashwatdwi176 <dwivedishashwat176@gmail.com>
2024-02-23 01:00:35 +0530

    Added project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [length , setLength] = useState(8)
+  const [numberAllowed , setNumberAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
